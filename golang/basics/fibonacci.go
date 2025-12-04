e464320450c2282017eb7745fdffde87fcb6f306
Muhammad Awais <awaisshahid965@gmail.com>
2025-12-04 23:59:31 +0500

    feat: add fibonacci series code

--- golang/basics/fibonacci.go
+++ golang/basics/fibonacci.go
@@ -4,10 +4,23 @@ import "fmt"
 
 func fibonacci(n int) []int {
 	fmt.Println("Calculating Fibonacci sequence...")
-	return []int{}
+
+	if n <= 0 {
+		return []int{}
+	}
+
+	fibSeq := make([]int, n+1)
+	fibSeq[0] = 0
+	fibSeq[1] = 1
+
+	for i := 2; i < n; i++ {
+		fibSeq[i] = fibSeq[i-1] + fibSeq[i-2]
+	}
+
+	return fibSeq
 }
 
 func main() {
-	n := 10
+	n := 1
 	fmt.Printf("First %d Fibonacci numbers: %v\n", n, fibonacci(n))
 }
