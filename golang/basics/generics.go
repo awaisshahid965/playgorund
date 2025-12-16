7ccb970d850270557ef05d8cae4d2c86b611561c
Muhammad Awais <awaisshahid965@gmail.com>
2025-12-16 23:23:26 +0500

    feat: implement traversal function for generics in Go

--- golang/basics/generics.go
+++ golang/basics/generics.go
@@ -1,1 +1,25 @@
 // generics.go
+package main
+
+import (
+	"fmt"
+)
+
+func traverse[T int | string](slice []T) {
+	for _, v := range slice {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	fmt.Println("Practicing Go Generics...\n")
+
+	ints := []int{1, 2, 3, 4, 5}
+	strs := []string{"Hello", "Generics", "in", "Go"}
+
+	fmt.Printf("Traversing int slice:\n")
+	traverse(ints)
+
+	fmt.Printf("\nTraversing string slice:\n")
+	traverse(strs)
+}
