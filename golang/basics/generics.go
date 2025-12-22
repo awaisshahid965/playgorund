224541a72eb1b558d4edc906dcf3e184a21ea187
Muhammad Awais <awaisshahid965@gmail.com>
2025-12-22 23:59:00 +0500

    Update generics.go

--- golang/basics/generics.go
+++ golang/basics/generics.go
@@ -16,7 +16,7 @@ func main() {
 	ints := []int{1, 2, 3, 4, 5}
 	strs := []string{"Hello", "Generics", "in", "Golang"}
 
-	fmt.Printf("Traversing int slice:\n")
+	fmt.Printf("\nTraversing int slice:\n")
 	traverse(ints)
 
 	fmt.Printf("\nTraversing string slice:\n")
